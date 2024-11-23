ee142eb8007ea3acbc0dc1dd59c7b827ccb39247
Naveen Reddy Nagulapally <naveenreddynagulapally@mac.home>
2024-11-23 15:54:29 -0500

    updated fetching userfunction

---
+++ SnapScroll_NRR/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  SnapScroll_NRR
+//
+//  Created by Naveen Reddy Nagulapally on 2024-11-23.
+//
+
+import Firebase
+
+let COLLECTIONS_USERS = Firestore.firestore().collection("users")
--- SnapScroll_NRR/ViewModel/Authentication/AuthViewModel.swift
+++ SnapScroll_NRR/ViewModel/Authentication/AuthViewModel.swift
@@ -17,6 +17,7 @@ class AuthViewModel: ObservableObject {
     
     init() {
         userSession = Auth.auth().currentUser
+        fetchUser()
     }
     
     func login(withEmail email: String, password: String) {
@@ -52,7 +53,7 @@ class AuthViewModel: ObservableObject {
                             "profileImageUrl": imageUrl,
                             "uid": user.uid]
                 
-                Firestore.firestore().collection("users").document(user.uid).setData(data){ _ in                print("Successfully uploaded user data to firestore!")
+                COLLECTIONS_USERS.document(user.uid).setData(data){ _ in                print("Successfully uploaded user data to firestore!")
                     self.userSession = user
                 }
             }
@@ -71,7 +72,11 @@ class AuthViewModel: ObservableObject {
     }
     
     func fetchUser() {
-        print("fetchUser")
+        guard let uid = userSession?.uid else { return }
+        COLLECTIONS_USERS.document(uid).getDocument { snapshot, _ in
+            print(snapshot?.data())
+            
+        }
         
     }
     
