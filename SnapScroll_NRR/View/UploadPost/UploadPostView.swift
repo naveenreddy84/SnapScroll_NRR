b06c1abdb19f43430723dccf4a56c8d0056654e2
Rajesh <begarirajesh83@gmail.com>
2024-11-27 16:31:26 -0800

    upload image complete changes

--- SnapScroll_NRR/View/UploadPost/UploadPostView.swift
+++ SnapScroll_NRR/View/UploadPost/UploadPostView.swift
@@ -12,6 +12,7 @@ struct UploadPostView: View {
     @State var postImage: Image?
     @State var captionText = ""
     @State var imagePickerPresented = false
+    @ObservedObject var viewModel = UploadPostViewModel()
     
     var body: some View {
         VStack{
@@ -42,7 +43,11 @@ struct UploadPostView: View {
             }.padding()
             
             
-            Button(action: {} , label: {
+            Button(action: {
+                if let image = selectedImage{
+                    viewModel.uploadPost(caption: captionText, image: image)
+                }
+            } , label: {
                 Text("Share")
                     .font(.system(size: 16, weight: .semibold))
                     .frame(width: 360, height: 50)
