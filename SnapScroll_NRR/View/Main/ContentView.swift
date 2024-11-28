a0fefbaa65ee0f86ee09391700f42e3ec09a589a
Rajesh <begarirajesh83@gmail.com>
2024-11-27 20:51:55 -0800

    splash screen added

--- SnapScroll_NRR/View/Main/ContentView.swift
+++ SnapScroll_NRR/View/Main/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Naveen Reddy Nagulapally on 2024-10-21.
 //
 
+/*
 import SwiftUI
 
 struct ContentView: View {
@@ -28,3 +29,73 @@ struct ContentView: View {
     
     
 }
+ */
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    @State private var showSplashScreen = true
+    @State private var selectedIndex = 0
+
+    var body: some View {
+        ZStack {
+            
+            if !showSplashScreen {
+                Group {
+                    if viewModel.userSession == nil {
+                      
+                        LoginPage()
+                    } else {
+                        if let user = viewModel.currentUser {
+                            
+                            MainTabView(user: user, selectedIndex: $selectedIndex)
+                        }
+                    }
+                }
+            }
+
+           
+            if showSplashScreen {
+                SplashScreenView()
+                    .onAppear {
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            withAnimation {
+                                showSplashScreen = false
+                            }
+                        }
+                    }
+            }
+        }
+        .animation(.easeInOut, value: showSplashScreen)
+    }
+}
+
+
+struct SplashScreenView: View {
+    var body: some View {
+        VStack {
+           
+            Text("SnapScroll")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.primary)
+                .padding()
+
+          
+            Image("SnapScrollLogo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+    }
+}
+
+
+
+
