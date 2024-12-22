0f91bc149a3067d6ec0fd73408bf48f87e017ff1
Ravali <ravalicanada3166@gmail.com>
2024-12-22 16:00:58 -0500

    Changed authentication files login, resetpassword, signup for better UI

--- SnapScroll_NRR/ViewModel/Authentication/AuthViewModel.swift
+++ SnapScroll_NRR/ViewModel/Authentication/AuthViewModel.swift
@@ -1,10 +1,3 @@
-//
-//  AuthViewModel.swift
-//  SnapScroll_NRR
-//
-//  Created by Prashanth Muppa on 11/10/24.
-//
-
 import SwiftUI
 import Firebase
 import FirebaseAuth
@@ -12,7 +5,8 @@ import FirebaseAuth
 class AuthViewModel: ObservableObject {
     
     @Published var userSession: FirebaseAuth.User?
-    @Published var currentUser : User?
+    @Published var currentUser: User?
+    @Published var errorMessage: String?  // Property to store the error message
     
     static let shared = AuthViewModel()
     
@@ -24,6 +18,8 @@ class AuthViewModel: ObservableObject {
     func login(withEmail email: String, password: String) {
         Auth.auth().signIn(withEmail: email, password: password) { result, error in
             if let error = error {
+                // Capture and set the error message if login fails
+                self.errorMessage = "Invalid username/password. Please try again."
                 print("DEBUG: Login Failed \(error.localizedDescription)")
                 return
             }
@@ -32,22 +28,25 @@ class AuthViewModel: ObservableObject {
             self.userSession = user
             self.fetchUser()
             
+            // Reset error message after successful login
+            self.errorMessage = nil
         }
     }
     
     func register(withEmail email: String, password: String,
                   image: UIImage?, fullname: String, username: String) {
-        guard let image = image else {return}
+        guard let image = image else { return }
         
         ImageUploader.uploadImage(image: image, type: .profile) { imageUrl in
             
-            Auth.auth().createUser(withEmail: email, password: password){ result, error in
+            Auth.auth().createUser(withEmail: email, password: password) { result, error in
                 if let error = error {
-                    print(error.localizedDescription)
+                    self.errorMessage = "Error registering: \(error.localizedDescription)"
+                    print("DEBUG: Registration Failed \(error.localizedDescription)")
                     return
                 }
                 
-                guard let user = result?.user else {return}
+                guard let user = result?.user else { return }
                 print("Successfully registered user!")
                 
                 let data = ["email": email,
@@ -56,33 +55,40 @@ class AuthViewModel: ObservableObject {
                             "profileImageUrl": imageUrl,
                             "uid": user.uid]
                 
-                COLLECTIONS_USERS.document(user.uid).setData(data){ _ in                print("Successfully uploaded user data to firestore!")
+                COLLECTIONS_USERS.document(user.uid).setData(data) { _ in
+                    print("Successfully uploaded user data to Firestore!")
                     self.userSession = user
                     self.fetchUser()
+                    
+                    // Reset error message after successful registration
+                    self.errorMessage = nil
                 }
             }
         }
     }
     
     func signout() {
-       
         self.userSession = nil
         try? Auth.auth().signOut()
-        
     }
     
-    func resetPassword() {
-        print("reset password")
+    func resetPassword(withEmail email: String) {
+        Auth.auth().sendPasswordReset(withEmail: email) { error in
+            if let error {
+                self.errorMessage = error.localizedDescription
+                print("error reset password: \(error.localizedDescription)")
+            } else {
+                self.errorMessage = "A password reset link has been sent to your email."
+            }
+        }
     }
     
     func fetchUser() {
         guard let uid = userSession?.uid else { return }
         COLLECTIONS_USERS.document(uid).getDocument { snapshot, _ in
-           guard let user = try? snapshot?.data(as: User.self) else { return }
+            guard let user = try? snapshot?.data(as: User.self) else { return }
             self.currentUser = user
-            
         }
-        
     }
-    
 }
+
